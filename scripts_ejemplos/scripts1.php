cb6fc509ec8fc95be6bc6b63cce735485a240ecf
juanda <jadavid646@gmail.com>
2019-03-14 21:58:06 -0500

    script1

---
+++ scripts_ejemplos/scripts1.php
@@ -0,0 +1,22 @@
+
+<center> MI PRIMER SCRIPT </center>
+<?php
+
+// Mi primer script en php 
+
+// Desarrollado por: Juan David Achicanoy
+
+
+$nombre="juan";
+$apellido="david";
+$email="jadavid646@gmail.com";
+
+echo"<center>html en php</center>";
+echo "Bienvenidos:" .$nombre."".$apellido."<br>";
+
+echo $nombre."<br>";
+echo $apellido."<br>";
+echo $email."<br>";
+
+?>
+<center> FIN DEL SCRIP</center>
