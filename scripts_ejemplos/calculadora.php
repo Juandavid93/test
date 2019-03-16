2042083cd0601e132a50ee3b45b18a95fa568c91
juanda <jadavid646@gmail.com>
2019-03-15 20:02:42 -0500

    calculadora

---
+++ scripts_ejemplos/calculadora.php
@@ -0,0 +1,8 @@
+<?php
+$num1 = $_POST["n1"];
+$num2 = $_POST["n1"];
+$oper = $_POST["tipo_op"];
+
+echo "numero1:".$num1."<br>";
+echo "numero2:".$num2."<br>";
+echo "tipo operacion:"$oper;
---
+++ scripts_ejemplos/scriptoperaciones.php
@@ -0,0 +1,38 @@
+<?php
+
+// variables
+$a1=10;
+$a2=8;
+
+// proceso
+$suma = $a1 + $a2;
+echo $suma;
+
+echo "</br>";
+echo "</br>";
+
+// resta
+
+$resta =  $a1 - $a2;
+
+echo $resta;
+echo "</br>";
+echo "</br>";
+
+// multiplicacion
+
+$multiplicar = $a1 * $a2;
+echo $multiplicar;
+echo "</br>";
+echo "</br>";
+
+// proceso division
+
+$dividir = $a1 / $a2;
+echo $dividir;
+
+echo "</br>";
+echo "</br>";
+
+
+?>
