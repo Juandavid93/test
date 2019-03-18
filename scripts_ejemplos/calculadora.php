a684c2993d4fd50f8a22c132bc5e5031994b231c
juan david <jadavid646@gmail.com>
2019-03-17 20:09:07 -0500

    calculadora

--- scripts_ejemplos/calculadora.php
+++ scripts_ejemplos/calculadora.php
@@ -1,8 +1,24 @@
 <?php
 $num1 = $_POST["n1"];
-$num2 = $_POST["n1"];
+$num2 = $_POST["n2"];
 $oper = $_POST["tipo_op"];
+$resul=0;
 
 echo "numero1:".$num1."<br>";
 echo "numero2:".$num2."<br>";
-echo "tipo operacion:"$oper;
+echo "tipo_op:".$oper."<br>";
+
+if($oper=="1")
+$resul = $num1 + $num2;
+else
+if($oper=="2")
+$resul = $num1 - $num2;
+else
+if($oper=="3")
+$resul = $num1 * $num2;
+else
+$resul = $num1/$num2;
+
+echo"Resultado:".$resul;
+
+?>
